3d6778d1144f7a2b4530d9ca3215015bd324fbc4
Shiham123 <s36651786@gmail.com>
2023-11-24 10:59:05 +0600

    footer section added

--- src/Section/FooterSection/Footer.jsx
+++ src/Section/FooterSection/Footer.jsx
@@ -1,5 +1,24 @@
+import FooterLogo from '../../assets/logo.png';
+
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <section>
+      <footer className="footer footer-center p-10 bg-colorOne text-colorTwo">
+        <aside>
+          <div>
+            <img width={50} className="rounded-full" src={FooterLogo} alt="" />
+          </div>
+          <p className="font-cinzel font-semibold py-4 text-xl md:text-2xl lg:text-3xl tracking-widest">
+            Survey website <br />
+            Providing reliable tech since 1992
+          </p>
+          <p className="font-cinzel font-semibold py-4 text-sm md:text-xl lg:text-2xl tracking-widest">
+            Copyright © 2023 - All right reserved
+          </p>
+        </aside>
+      </footer>
+    </section>
+  );
 };
 
 export default Footer;
